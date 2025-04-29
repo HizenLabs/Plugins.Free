ac81dedcbe0ff42927687514fa1c7e3d911e5018
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-04-28 22:48:38 -0400

    add required item properties for ItemManager.CreateById()

--- src/Carbon.Plugins/Active/AutoBuildSnapshot/AutoBuildSnapshot.PersistantItem.cs
+++ src/Carbon.Plugins/Active/AutoBuildSnapshot/AutoBuildSnapshot.PersistantItem.cs
@@ -1,4 +1,6 @@
-﻿namespace Carbon.Plugins;
+﻿using System.IO;
+
+namespace Carbon.Plugins;
 
 public partial class AutoBuildSnapshot
 {
@@ -9,14 +11,52 @@ public partial class AutoBuildSnapshot
     {
         private static readonly IPropertyMapping[] _mappings = new IPropertyMapping[]
         {
-            new PropertyMapping<Item, int>(i => i.info.itemid),
-            new PropertyMapping<Item, int>(i => i.amount),
-            new PropertyMapping<Item, Item.Flag>(i => i.flags, i => i.HasFlag(Item.Flag.IsLocked)),
-            new PropertyMapping<Item, string>(i => i.info.tag)
+            new PropertyMapping<Item, Item.Flag>(i => i.flags, i => i.flags != Item.Flag.None),
+            new PropertyMapping<Item, string>(i => i.info.tag, i => !string.IsNullOrEmpty(i.info.tag))
         };
 
         public PersistantItem() : base(_mappings)
         {
+        }
+
+        public int ItemID { get; private set; }
+
+        public int Amount { get; private set; }
+
+        public ulong SkinID { get; private set; }
+
+        public override void EnterPool()
+        {
+            base.EnterPool();
+
+            ItemID = default;
+        }
+
+        public override void Read(BinaryReader reader)
+        {
+            base.Read(reader);
+
+            ItemID = reader.ReadInt32();
+            Amount = reader.ReadInt32();
+            SkinID = reader.ReadUInt64();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            base.Write(writer);
+
+            writer.Write(ItemID);
+            writer.Write(Amount);
+            writer.Write(SkinID);
+        }
+
+        public override void Read(Item obj)
+        {
+            base.Read(obj);
+
+            ItemID = obj.info.itemid;
+            Amount = obj.amount;
+            SkinID = obj.skin;
         }
     }
 }
