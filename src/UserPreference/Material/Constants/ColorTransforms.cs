954cff913a7862aa520d8cf28a373784c7115ccf
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-05-10 15:53:59 -0400

    fix file name

--- src/UserPreference/Material/Constants/ColorTransforms.cs
+++ src/UserPreference/Material/Constants/ColorTransforms.cs
@@ -28,4 +28,24 @@ internal static class ColorTransforms
         -0.9691452513005321d, 1.8758853451067872d, 0.04156585616912061d,
         0.05562093689691305d, -0.20395524564742123d, 1.0571799111220335d
     );
+
+    /// <summary>
+    /// 3×3 matrix to convert CIE XYZ to CAM16 RGB.
+    /// </summary>
+    public static readonly ColorConversionMatrix XyzToCam16rgb = new
+    (
+        0.401288, 0.650173, -0.051461,
+        -0.250268, 1.204414, 0.045854,
+        -0.002079, 0.048952, 0.953127
+    );
+
+    /// <summary>
+    /// 3×3 matrix to convert CAM16 RGB to CIE XYZ.
+    /// </summary>
+    public static readonly ColorConversionMatrix Cam16rgbToXyz = new
+    (
+        1.8620678, -1.0112547, 0.14918678,
+        0.38752654, 0.62144744, -0.00897398,
+        -0.01584150, -0.03412294, 1.0499644
+    );
 }
