2ec892e3973bd78b61812d58f8e725076a9f1d13
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-05-11 22:16:00 -0400

    test ToCam16PreAdaptRgb

--- src/Carbon.Tests/Extensions/UserPreference/Material/Structs/CieXyzTests.cs
+++ src/Carbon.Tests/Extensions/UserPreference/Material/Structs/CieXyzTests.cs
@@ -26,4 +26,27 @@ public class CieXyzTests
     }
 
     #endregion
+
+    #region Conversions
+
+    [TestMethod]
+    [DataRow(0d, 0d, 0d, 0d, 0d, 0d)]
+    [DataRow(0.3575761, 0.7151522, 0.119192, 0.6023701672, 0.7773668528, 0.1478797584)]
+    [DataRow(0.95047, 1d, 1.08883, 0.97555292473, 1.01646898486, 1.0847692442)]
+    public void ToCam16PreAdaptRgb_ShouldBeCorrect(double x, double y, double z, double cam16R, double cam16G, double cam16B)
+    {
+        // Arrange
+        CieXyz cieXyz = new(x, y, z);
+        Cam16PreAdaptRgb expected = new(cam16R, cam16G, cam16B);
+        
+        // Act
+        Cam16PreAdaptRgb actualCam16 = cieXyz.ToCam16PreAdaptRgb();
+
+        // Assert
+        Assert.AreEqual(expected.R, actualCam16.R, 0.0001);
+        Assert.AreEqual(expected.G, actualCam16.G, 0.0001);
+        Assert.AreEqual(expected.B, actualCam16.B, 0.0001);
+    }
+
+    #endregion
 }
