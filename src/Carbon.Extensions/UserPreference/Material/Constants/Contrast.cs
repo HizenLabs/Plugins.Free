753a2e49b16c118f5ac5314319256f83dd423f39
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-05-17 09:50:27 -0400

    add contrast constants

---
+++ src/Carbon.Extensions/UserPreference/Material/Constants/Contrast.cs
@@ -0,0 +1,10 @@
+﻿namespace HizenLabs.Extensions.UserPreference.Material.Constants;
+
+internal static class Contrast
+{
+    public static readonly double Standard = 0.0;
+
+    public static readonly double Medium = 0.5;
+
+    public static readonly double High = 1.0;
+}
